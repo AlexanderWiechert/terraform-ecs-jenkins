3fbe3d278921e26b61139aa628012ef13a18ca40
awiechert <awiechert@elastic2ls.com>
2023-08-09 10:34:39 +0200

    fix role permission & group policy attachment

--- terraform/modules/ecs/iam.tf
+++ terraform/modules/ecs/iam.tf
@@ -1,28 +1,62 @@
-data "aws_iam_policy_document" "jenkins" {
-  statement {
-    sid = "1"
-
-    actions = [
-      "*"
+resource "aws_iam_group_policy" "jenkins" {
+  name  = "jenkins"
+  group = aws_iam_group.jenkins.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:**",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "ecs:**",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "ecr:**",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
     ]
-
-    resources = [
-      "*",
-    ]
-  }
+  })
 }
+
+resource "aws_iam_group_policy_attachment" "jenkins" {
+  group      = aws_iam_group.jenkins.name
+  policy_arn = aws_iam_group_policy.jenkins.arn
+}
+### DEV ###
+
+resource "aws_iam_group" "jenkins" {
+  name = "jenkins"
+}
+
+resource "aws_iam_group_membership" "jenkins" {
+  name = "jenkins"
+
+  users = [
+    aws_iam_user.jenkins_dev.name,
+    aws_iam_user.jenkins_qa.name,
+    aws_iam_user.jenkins_dns.name,
+  ]
+
+  group = aws_iam_group.jenkins.name
+}
+
+
 
 ### DEV ###
+
 resource "aws_iam_user" "jenkins_dev" {
   name = "jenkins_mb"
-  lifecycle {
-    prevent_destroy = "true"
-  }
-}
-
-resource "aws_iam_user_policy" "jenkins_dev" {
-  policy = data.aws_iam_policy_document.jenkins.json
-  user   = aws_iam_user.jenkins_dev.name
   lifecycle {
     prevent_destroy = "true"
   }
@@ -36,18 +70,9 @@ resource "aws_iam_access_key" "jenkins_dev" {
 }
 ### ###
 
- ### QA ###
+### QA ###
  resource "aws_iam_user" "jenkins_qa" {
    name     = "jenkins_mb"
-   provider = aws.aws_qa
-   lifecycle {
-     prevent_destroy = "true"
-   }
- }
-
- resource "aws_iam_user_policy" "jenkins_qa" {
-   policy   = data.aws_iam_policy_document.jenkins.json
-   user     = aws_iam_user.jenkins_qa.name
    provider = aws.aws_qa
    lifecycle {
      prevent_destroy = "true"
@@ -61,20 +86,11 @@ resource "aws_iam_access_key" "jenkins_dev" {
      prevent_destroy = "true"
    }
  }
- ### ###
+### ###
 
 ### DNS ###
 resource "aws_iam_user" "jenkins_dns" {
   name     = "jenkins_mb"
-  provider = aws.aws_dns
-  lifecycle {
-    prevent_destroy = "true"
-  }
-}
-
-resource "aws_iam_user_policy" "jenkins_dns" {
-  policy   = data.aws_iam_policy_document.jenkins.json
-  user     = aws_iam_user.jenkins_dns.name
   provider = aws.aws_dns
   lifecycle {
     prevent_destroy = "true"
